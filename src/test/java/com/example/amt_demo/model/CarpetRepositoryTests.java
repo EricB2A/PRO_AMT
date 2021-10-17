6373cea4bf6d8f6f7da37d06bbc0e2a7fb88680e
Ovich <stefanteofanovic@hotmail.com>
2021-10-17 18:49:15 +0200

    Test CI Tests in Runner

--- src/test/java/com/example/amt_demo/model/CarpetRepositoryTests.java
+++ src/test/java/com/example/amt_demo/model/CarpetRepositoryTests.java
@@ -55,8 +55,7 @@ public class CarpetRepositoryTests {
 
     @Test
     public void CarpetRepositoryTests_countMockElement() {
-        long count = carpetRepository.count();
-        Assertions.assertEquals(10, count);
+        Assertions.assertEquals(10, carpetRepository.count());
     }
 
     @AfterAll
