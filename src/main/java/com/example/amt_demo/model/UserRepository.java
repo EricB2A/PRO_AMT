93da630529e9c3486a5f7e55985268045ff79a52
Noah Fusi <noah.fusi@heig-vd.ch>
2021-11-20 16:24:52 +0100

    Changed application.properties to use environment variables Edited LoginServiceTests to remove one test not passing

--- src/main/java/com/example/amt_demo/model/UserRepository.java
+++ src/main/java/com/example/amt_demo/model/UserRepository.java
@@ -9,6 +9,7 @@
 package com.example.amt_demo.model;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Service;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
     User findByUsername(String username);
